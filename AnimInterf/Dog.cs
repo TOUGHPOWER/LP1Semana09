18839d9532f557cda3e201cb6bd05a9bb9bd5748
Pedroosorio97 <pedromrnosorio97@gmail.com>
2022-05-22 20:34:58 +0100

    Created Animal, Dog, Cat, Bat and Bee classes

---
+++ AnimInterf/Dog.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AnimInterf
+{
+    public class Dog:Animal
+    {
+        public override string Sound()
+        {
+            return base.Sound() + "Woof";
+        }
+    }
+}
